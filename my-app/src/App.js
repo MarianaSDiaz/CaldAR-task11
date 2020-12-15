700febcfc05c670141cde415ed9b74ce65e2ac0b
MarianaSDiaz <maruuus.95@gmail.com>
2020-12-15 12:02:32 -0300

    Construction Company: first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './App.css';
+import Form from './components/Form/Form';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Construction Companies</h1>
+      </div>
+    );
+  }
+}
+export default App;
